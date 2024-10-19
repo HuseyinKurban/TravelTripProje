65468948e87c8a71774c32366c1614f03ec347f2
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-10-19 22:48:49 +0300

    Add project files.

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 
 namespace TravelTripProje.Controllers
@@ -10,14 +11,35 @@ namespace TravelTripProje.Controllers
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+
+            var deger = c.Blogs.ToList();
+            return View(deger);
         }
 
         public ActionResult About()
         {
             return View();
         }
+
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+
+            var degerler = c.Blogs.OrderByDescending(x => x.ID)
+                                  .Skip(2) // Sondan 3. öğeye ulaşmak için 2 öğe atla
+                          .Take(1) // Sadece 1 öğe al
+                          .ToList();
+            return PartialView(degerler);
+        }
+
+  
     }
 }
