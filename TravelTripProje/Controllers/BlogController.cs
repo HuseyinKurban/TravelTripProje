2c5e529ecfd3fc8791421244d19d3107160e25cc
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-10-19 20:32:29 +0300

    Add project files.

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -10,23 +10,26 @@ namespace TravelTripProje.Controllers
     public class BlogController : Controller
     {
         // GET: Blog
-        Context c=new Context();
-
+        Context c = new Context();
+        BlogYorum by = new BlogYorum();
         public ActionResult Index()
         {
-            var bloglar=c.Blogs.ToList();
-            return View(bloglar);
+            // var bloglar=c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3=c.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList();
+            by.Deger4=c.Yorumlars.OrderByDescending(x => x.ID).Take(3).ToList();
+            return View(by);
         }
-        BlogYorum by=new BlogYorum();
+
         public ActionResult BlogDetay(int id)
         {
 
             //var blogbul=c.Blogs.Where(x=>x.ID==id).ToList();
-            by.Deger1=c.Blogs.Where(x=>x.ID==id).ToList();
-            by.Deger2=c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
             return View(by);
         }
 
-       
+
     }
 }
--- TravelTripProje/Models/Siniflar/BlogYorum.cs
+++ TravelTripProje/Models/Siniflar/BlogYorum.cs
@@ -9,6 +9,9 @@ namespace TravelTripProje.Models.Siniflar
     {
         public IEnumerable<Blog> Deger1 { get; set; }
         public IEnumerable<Yorumlar> Deger2 { get; set; }
+        public IEnumerable<Blog> Deger3 { get; set; }
+        public IEnumerable<Yorumlar> Deger4 { get; set; }
+
 
     }
 }
